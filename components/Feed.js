296b0c9486bd538d95b72f242bcc4a1b821647ed
Elfun <sparkstorms2@gmail.com>
2022-07-23 11:43:48 +0900

    Retriving Posts

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,26 @@
 import { SparklesIcon } from "@heroicons/react/outline"
+import { useEffect, useState } from "react"
 import Tweet from "./Tweet"
+import { db } from "../firebase"
+import { onSnapshot, collection, query, orderBy } from "firebase/firestore"
+import Post from "./Post"
 
 const Feed = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(
+        ()=>
+            onSnapshot(
+                query(collection(db,"posts"), orderBy("timestamp","desc")),
+                (snapshot) => {
+                    setPosts(snapshot.docs)
+                }
+            ),
+        [db]
+    )
+
+    console.log(posts)
+
     return (
         <div className="text-white flex-grow border-l
         border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
@@ -16,6 +35,11 @@ const Feed = () => {
             </div>
 
             <Tweet />
+            <div className="pb-72">
+                {posts.map(post => (
+                    <Post key={post.id} id={post.id} post={post.data()} />
+                ))}
+            </div>
         </div>
     )
 }
